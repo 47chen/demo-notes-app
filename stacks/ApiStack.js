fa7ada3fc1bc081cf79a4ae429200403b07ada29
47chen <shihchichen1010@vt.edu>
2022-09-12 01:17:42 -0400

    Add an API to Create a Note + Test the API on SST Console

---
+++ stacks/ApiStack.js
@@ -0,0 +1,32 @@
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
+    routes: {
+      "POST /notes": "functions/create.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  // print url of our API as an output by calling
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
