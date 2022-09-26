937531ca87d0eb4a60314cdc721b14a551b0c848
47chen <shihchichen1010@vt.edu>
2022-09-26 19:09:39 -0400

    Adding Unit tests

--- services/functions/billing.js
+++ services/functions/billing.js
@@ -3,6 +3,9 @@ import handler from "../util/handler";
 import { calculateCost } from "../util/cost";
 
 export const main = handler(async (event) => {
+  // get storage and source from the request body
+  // storage -> the number of notes that users would like to store in their account
+  // source -> the Stripe token for the card that we are going to charge
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
   const description = "Scratch charge";
---
+++ services/test/cost.test.js
@@ -0,0 +1,33 @@
+import { expect, test } from "vitest";
+import { calculateCost } from "../util/cost";
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+// They are testing the different tiers of our pricing structure.
+// We test the case where a user is trying to store 10, 100, and 101 notes.
+// And comparing the calculated cost to the one we are expecting.
