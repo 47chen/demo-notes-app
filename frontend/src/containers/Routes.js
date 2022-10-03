72c554fab1e326cf126eb103ff9336c75cd4d953
47chen <shihchichen1010@vt.edu>
2022-10-03 00:50:42 -0400

    Create a Login page(container) with new route

--- frontend/src/containers/Routes.js
+++ frontend/src/containers/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
+import Login from "./Login";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
       {/* catch all unmatched routes */}
       {/* This needs to always be the last route in the <Routes> block. */}
