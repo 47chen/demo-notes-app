7d1427253db49b00d92e275db89b361d5db7fd30
47chen <shihchichen1010@vt.edu>
2022-10-02 23:33:04 -0400

    Handle 404s

--- frontend/src/containers/Routes.js
+++ frontend/src/containers/Routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import NotFound from "./NotFound";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+      {/* catch all unmatched routes */}
+      {/* This needs to always be the last route in the <Routes> block. */}
+      {/* You can think of it as the route that handles requests in case all the other routes before it have failed. */}
     </Routes>
   );
 }
