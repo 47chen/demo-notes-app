4e3c430c098414e44a25fab6a6b0982e919089d8
47chen <shihchichen1010@vt.edu>
2022-10-06 00:37:36 -0400

    Display all the notes list

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useAppContext } from "../lib/contextLib";
+import { onError } from "../lib/errorLib";
 import "./Home.css";
 
 //This simply renders our homepage given that the user is not currently signed in.
 export default function Home() {
-  return (
-    <div className="Home">
+  const [notes, setNotes] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const renderNotesList = (notes) => {
+    return null;
+  };
+
+  const renderLander = () => {
+    return (
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
       </div>
+    );
+  };
+
+  const renderNotes = () => {
+    return (
+      <div className="notes">
+        <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  };
+
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
     </div>
   );
 }
